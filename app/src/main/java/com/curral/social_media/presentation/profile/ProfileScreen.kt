14a74d7d5ab3928aafe8e3029b97123a464800c8
Gabriel Brisson <gabrielbrisson02@gmail.com>
2023-04-18 12:08:05 -0300

    WIP Profile Screen

--- app/src/main/java/com/curral/social_media/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/curral/social_media/presentation/profile/ProfileScreen.kt
@@ -1,11 +1,40 @@
 package com.curral.social_media.presentation.profile
 
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.curral.social_media.R
 import com.curral.social_media.ui.theme.SocialMediaTheme
 
 @Composable
@@ -18,13 +47,62 @@ fun ProfileScreen(
     ProfileScreen(modifier = modifier, uiState = uiState, onBack = onBack)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun ProfileScreen(
     modifier: Modifier = Modifier,
     uiState: ProfileUiState,
     onBack: () -> Unit,
 ) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(title = {
+                Text(
+                    text = "",
+                    fontWeight = FontWeight.SemiBold,
+                )
+            },
+                navigationIcon = {
+                    Icon(imageVector = Icons.Rounded.ArrowBack, contentDescription = "")
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Filled.Add, contentDescription = "Add Button")
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = modifier
+                .padding(paddingValues)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+                    .border(4.dp, Color.White, CircleShape),
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data("")
+                    .crossfade(true)
+                    .build(),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.profileplaceholder),
+                contentDescription = ""
+            )
+            Text(text = "Gabriel", style = MaterialTheme.typography.titleLarge)
+            Text(
+                modifier = Modifier.padding(top = 46.dp),
+                text = "Friends",
+                style = MaterialTheme.typography.titleLarge
+            )
+            LazyColumn(){
 
+            }
+        }
+    }
 }
 
 @Preview
